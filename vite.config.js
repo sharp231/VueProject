815e214a37faae11038564311a8b38fa1866ee89
sharp231 <yuudai07ya@gmail.com>
2024-02-19 15:28:38 +0900

    🐛 改善

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': './src',
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
